b9c118143b97edc0c70b11e179168ba740c78caf
HamzaAkb <hamzawakbar@gmail.com>
2025-06-25 23:52:04 +0500

    change in enviroment variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
-  console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
-
   return (
     <>
-      {process.env.NODE_ENV === 'development' ? (
+      {process.env.NEXT_PUBLIC_ENV === 'development' ? (
         <div>Development</div>
       ) : (
         <div>Production</div>
